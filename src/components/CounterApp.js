5ccd1a59b4d382024522a04f848563fb527ad125
Vivek Jadhav <vivek.jadhav200127@gmail.com>
2025-11-10 21:56:15 +0530

    counter rendering issue

--- src/components/CounterApp.js
+++ src/components/CounterApp.js
@@ -15,7 +15,7 @@ function CounterApp() {
     }
     return (
         <div>
-            <h1>Count: {count}</h1>
+            <h1>{count}</h1>
             <button onClick={increment}>Increment</button>
             <button onClick={decrement}>Decrement</button>
         </div>
